8050a8a759a0055929a088f15c324dff7cc1e4d3
Ho-JunYu <hojoon1011@gmail.com>
2025-01-25 21:19:40 +0900

    [고객] 로그인 회원가입 기능 구현

---
+++ user-api/src/main/java/com/zerobase/user/config/filter/CustomerFilter.java
@@ -0,0 +1,32 @@
+package com.zerobase.user.config.filter;
+
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import com.zerobase.domain.domain.common.UserVo;
+import com.zerobase.user.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/customer/*")
+@RequiredArgsConstructor
+public class CustomerFilter implements Filter {
+
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final CustomerService customerService;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        String token = req.getHeader("X-AUTH-TOKEN");
+        if (!jwtAuthenticationProvider.validateToken(token)) {
+            throw new ServletException("Invalid Access");
+        }
+        UserVo vo = jwtAuthenticationProvider.getUserVo(token);
+        customerService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(() -> new ServletException("Invalid access"));
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
